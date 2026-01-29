4ff6462013f5c64e51a47c11e532c33ea5a2893e
Przemysław Psuja <przemyslaw.psuja@gmail.com>
2026-01-29 13:24:50 +0100

    Created a test class to check is webdriver work properly

---
+++ Tests/UI/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using SeleniumFrameworkProject.Core;
+using SeleniumFrameworkProject.Core.Configuration;
+
+namespace SeleniumFrameworkProject.Tests.UI
+{
+    [TestFixture]
+    public class TestClass
+    {
+        public TestSettings settings = TestSettings.Instance;
+
+        [Test]
+        public void Test()
+        {
+            var driver = DriverFactory.GetDriver();
+            driver.Navigate().GoToUrl(settings.BaseUrl);
+
+        }
+    }
+}
